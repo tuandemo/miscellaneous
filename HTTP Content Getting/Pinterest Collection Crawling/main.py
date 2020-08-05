72291448343a81ed32165401fb4417d3b089f716
Tuanm <tuan.dm185419@sis.hust.edu.vn>
2020-08-05 20:20:09 +0700

    add pinterest image local saving

--- HTTP Content Getting/Pinterest Collection Crawling/main.py
+++ HTTP Content Getting/Pinterest Collection Crawling/main.py
@@ -5,7 +5,6 @@ import requests
 def main(argv):
 	username = argv[1]
 	collection = argv[2]
-	filename = argv[3]
 	url = 'https://www.pinterest.com/' + username + '/' + collection
 	response = requests.get(url)
 	try:
@@ -23,17 +22,26 @@ def main(argv):
 		content = content[srcset:]
 		content = content[content.find('.jpg 1x') + 9:]
 		img += [content[:content.find('.jpg 2x') + 4]]
-		data = '\n'.join(img)
+	if len(argv) == 3:
+		pattern = r'(/[\d\w]+.jpg)'
+		for i in img:
+			filename = str(re.findall(pattern, i)[0]).replace('/', '')
+			with open(filename, 'wb') as f:
+				f.write(requests.get(i).content)
+			print('%s saved.' % (filename))
+		sys.exit(0)
+	data = '\n'.join(img)
+	filename = argv[3]
 	with open(filename, 'w') as f:
 		print(data)
 		f.writelines(data)
 		print('All pictures of the %s\'s collection "%s" have been saved in %s.' % (username, collection, filename))
 
 def usage():
-	print('Usage: python3 main.py <username> <collection> <filename>')
+	print('Usage: python3 main.py <username> <collection> [filename]')
 
 if __name__ == '__main__':
-	if len(sys.argv) == 4:
+	if len(sys.argv) >= 3:
 		main(sys.argv)
 		sys.exit(0)
 	else:
